aaf8668db555038588b9a7b1f1f8c939b450b206
clementreiffers <reiffersclement@gmail.com>
2022-04-16 12:14:12 +0100

    verif si ça passe par une ville

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const createItemForIterate = n => n == 0 ? false : [-n, n + 1];
 
 const unfoldPopulation = R.unfold(createItemForIterate, -MAX_POPULATION);
 
-const villes = [
+const cities = [
     {x: 2, y: 5},
     {x: 6, y: 2},
     {x: 8, y: 1}
@@ -16,15 +16,43 @@ const villes = [
 
 let population = [];
 
+const getX = R.pipe(
+    R.pluck("x")
+)
+
+const getY = R.pipe(
+    R.pluck("y")
+)
+
+const checkIfSomeonePassThroughCity = (indiv, city) => {
+    if (R.and(R.equals(indiv[indiv.ways].x, city.x), R.equals(indiv[indiv.ways].y, city.y)))
+        indiv[indiv.ways].nbrCities++;
+}
+
+
+// const checkAutre = (indiv) => R.forEach(checkIfSomeonePassThroughCity(indiv[indiv.ways]), cities)
+
+
+// const checkIfSomeonePassThroughCity = (indiv) =>
+//     R.pluck('x', cities).some(r=> [indiv[indiv.ways].x].indexOf(r) >= 0)
+//     && R.pluck('y', cities).some(r=> [indiv[indiv.ways].y].indexOf(r) >= 0);
+
+
 const addStepToIndiv = (indiv) => {
     indiv[indiv.ways+1] = {
-        nbrCities: 0,
+        nbrCities: indiv[indiv.ways].nbrCities,
         dx:indiv[indiv.ways].dx,
         dy:indiv[indiv.ways].dy,
         x:indiv[indiv.ways].x+indiv[indiv.ways].dx,
         y:indiv[indiv.ways].y+indiv[indiv.ways].dy
     }
     indiv.ways++;
+    for(let city of cities)
+        checkIfSomeonePassThroughCity(indiv, city);
+    // R.any([1])([1]);
+    // R.any([indiv[indiv.ways].x], R.pluck('x', cities));
+    // console.log(R.pluck('x', cities));
+    // console.log([indiv[indiv.ways].x])
 }
 
 // on initialise ici chaque individus par une direction differente (ils vont demarrer au même point)
@@ -33,8 +61,8 @@ const createIndiv = n => population.push(
             ways:0,
             0: {
                 nbrCities: 0,
-                dx: parseInt(Math.random() * 10),
-                dy: parseInt(Math.random() * 10),
+                dx: parseInt(Math.random()*10),
+                dy: parseInt(Math.random()*10),
                 x: 0,
                 y: 0
             }
@@ -50,6 +78,8 @@ const mutatatePopulation = () => R.forEach(addStepToIndiv, population)
 
 mutatatePopulation();
 console.log(population);
+
+// checkAutre(R.nth(0, population))
 
 // const mutate = R.forEach()
 
