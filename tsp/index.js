e760de4bebe914e9e7d4d23b7af7924bb7a342b6
esme <ecole@esme.fr>
2022-04-19 17:51:38 +0200

    wip

--- tsp/index.js
+++ tsp/index.js
@@ -1,25 +1,43 @@
 import * as R from "ramda";
+// import * as p5 from "p5";
 
 const MAX_CITIES = 10;
+const MAX_POPULATION = 10;
 
 const cities = {};
-const indiv = [];
+const population = [];
 
-const createCity = (number) => cities[number] = {x:parseInt(Math.random()*10), y:parseInt(Math.random()*10)};
+const createCity = (number) => cities[number] = {x: parseInt(Math.random() * 10), y: parseInt(Math.random() * 10)};
 
 // fonction qui nous sert pour le unfold
 const createItemForIterate = n => n === 0 ? false : [-n, n + 1];
 
 // unfold c'est l'inverse d'un reduce, on cree ici une liste d'element a partir d'un chiffre
-const unfoldPopulation = R.unfold(createItemForIterate, -MAX_CITIES);
+const unfoldCities = R.unfold(createItemForIterate, -MAX_CITIES);
 
-const createCities = R.forEach(createCity, unfoldPopulation);
+const shuffle = R.sort(() => Math.random() - 0.5);
 
-const shuffle = (array) => array.sort(() => Math.random() - 0.5);
+const createIndiv = () => population.push({order: shuffle(unfoldPopulation), score: ""});
+const unfoldPopulation = R.unfold(createItemForIterate, -MAX_POPULATION);
 
-const createIndiv = () => indiv.push(shuffle(unfoldPopulation));
+const vectorNorm = (city1, city2) => Math.sqrt(Math.pow(city1.x-city2.x, 2) + Math.pow(city1.y-city2.y, 2));
+// const
 
-createIndiv()
-createIndiv()
+const createPopulation = R.append(R.forEach(createIndiv, unfoldCities));
 
-console.log(indiv)
+// a revoir
+const createCities = R.forEach(createCity, unfoldCities);
+
+
+const calculateScores = (array) => {
+    let score = 0;
+    for (let i in array) {
+        if (Number(i) + 1 < array.length)
+            score += vectorNorm(cities[R.nth(i, array)], cities[R.nth(Number(i) + 1, array)]);
+    }
+    return score;
+}
+
+console.log(calculateScores([1, 2, 3, 4]));
+
+// console.log(createPopulation(population));
