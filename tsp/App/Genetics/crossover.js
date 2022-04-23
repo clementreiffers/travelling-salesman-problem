211a371e6538955356880df7074bd95245d35371
QuentinM <59691442+Im-Rises@users.noreply.github.com>
2022-04-23 12:50:47 +0200

    Crossover done

--- tsp/App/Genetics/crossover.js
+++ tsp/App/Genetics/crossover.js
@@ -26,31 +26,13 @@ const crossOverPopulation = (population) =>
     R.last
   )(population);
 
-// const crossoverProbability = 1 / 5; //0.2 crossover percent possibility
-//
-// const shouldCrossover = R.pipe(Math.random, R.lt(1 - crossoverProbability));
-//
-// const crossOver = (reference, target, index) =>
-//   R.update(index, R.nth(index, reference), target);
-//
-// const magicMapper = (acc, value) => [
-//   value,
-//   crossOver(acc, value, getRandomIndex(acc))
-// ];
-//
-// const crossOverPopulation = (population) =>
-//   R.pipe(
-//     R.converge(R.mapAccum(magicMapper), [R.last, R.identity]),
-//     R.last
-//   )(population);
+export {crossOverPopulation};
 
-const population = [
-  [1, 2, 3, 4, 5, 6, 7, 8, 9],
-  [9, 8, 7, 6, 5, 4, 3, 2, 1],
-  [1, 2, 3, 4, 5, 6, 7, 8, 9],
-  [9, 8, 7, 6, 5, 4, 3, 2, 1]
-];
-
-// export {crossOverPopulation};
-
-console.log(crossOverPopulation(population));
+// console.log(
+//   crossOverPopulation([
+//     [1, 2, 3, 4, 5, 6, 7, 8, 9],
+//     [9, 8, 7, 6, 5, 4, 3, 2, 1],
+//     [1, 2, 3, 4, 5, 6, 7, 8, 9],
+//     [9, 8, 7, 6, 5, 4, 3, 2, 1]
+//   ])
+// );
