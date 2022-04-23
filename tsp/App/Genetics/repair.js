14d188979b0e8922e564c1b2483d5cfbd6f039ca
QuentinM <59691442+Im-Rises@users.noreply.github.com>
2022-04-23 14:34:59 +0200

    Repair corrected

--- tsp/App/Genetics/repair.js
+++ tsp/App/Genetics/repair.js
@@ -1,21 +1,23 @@
 import * as R from 'ramda';
-import {append} from 'ramda';
 
 // Repair :
 
-const listCities = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const listCities = (individual) => R.times(R.identity, individual.length);
 
-const getMissingCity = R.pipe(R.uniq, R.difference(listCities));
+const getMissingCity = (individual) =>
+  R.pipe(R.uniq, R.difference(listCities(individual)))(individual);
 
 const repair = R.converge(R.concat, [R.uniq, getMissingCity]);
 
 const repairPopulation = (population) => R.map(repair, population);
 
+export {repairPopulation};
+
 // console.log(
 //   repairPopulation([
-//     [1, 1, 3, 4, 5, 6, 7, 8, 9],
-//     [9, 9, 7, 6, 5, 4, 3, 2, 1],
-//     [1, 2, 2, 4, 5, 6, 7, 8, 9],
-//     [9, 8, 7, 6, 5, 7, 3, 2, 1]
+//     [0, 1, 1, 3, 4, 5, 6, 7, 8, 9],
+//     [0, 9, 9, 7, 6, 5, 4, 3, 2, 1],
+//     [0, 1, 2, 2, 4, 5, 6, 7, 8, 9],
+//     [0, 9, 8, 7, 6, 5, 7, 3, 2, 1]
 //   ])
 // );
--- tsp/index.js
+++ tsp/index.js
@@ -4,12 +4,14 @@ import {createPop} from './App/Genetics/initPopulation.js';
 import {createMap} from './App/Genetics/initMap.js';
 import sortListByScores from './App/Genetics/scores.js';
 import {mutatePopulation} from './App/Genetics/mutation.js';
-import {} from './App/Genetics/crossover.js';
-import {} from './App/Genetics/repair.js';
+import {crossOverPopulation} from './App/Genetics/crossover.js';
+import {repairPopulation} from './App/Genetics/repair.js';
 import {} from './App/Genetics/commonFunctions.js';
 
 const MAX_CITIES = 10;
 const MAX_POPULATION = 20;
+
+// listCities(MAX_POPULATION);
 
 let map = createMap(MAX_CITIES);
 let population = createPop(MAX_POPULATION)(MAX_CITIES);
@@ -18,3 +20,5 @@ const crossoverNumberCity = 3;
 
 population = sortListByScores(map)(population);
 console.log(population);
+
+// console.log(R.pluck('order', population));
