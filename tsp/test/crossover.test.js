6d116a91594ca51c9f645aa1280b86acf813a8b4
QuentinM <59691442+Im-Rises@users.noreply.github.com>
2022-04-24 01:55:15 +0200

    Added chaijs test

---
+++ tsp/test/crossover.test.js
@@ -0,0 +1,33 @@
+import {expect} from 'chai';
+import {crossOverPopulation} from '../App/Genetics/crossover.js';
+
+describe('../App/Genetics/crossover.js', () => {
+  describe('#crossOverPopulation()', () => {
+    it('Crossover function', () => {
+      expect(
+        crossOverPopulation([
+          [1, 2, 3, 4, 5, 6, 7, 8, 9],
+          [60, 70, 80, 90, 100, 110, 120, 130, 140]
+        ])
+      )
+        .to.be.an('array')
+        .to.be.lengthOf(2);
+      expect(
+        crossOverPopulation([
+          [1, 2, 3, 4, 5, 6, 7, 8, 9],
+          [60, 70, 80, 90, 100, 110, 120, 130, 140]
+        ])[0]
+      )
+        .to.be.an('array')
+        .to.be.lengthOf(9);
+      expect(
+        crossOverPopulation([
+          [1, 2, 3, 4, 5, 6, 7, 8, 9],
+          [60, 70, 80, 90, 100, 110, 120, 130, 140]
+        ])[1]
+      )
+        .to.be.an('array')
+        .to.be.lengthOf(9);
+    });
+  });
+});
