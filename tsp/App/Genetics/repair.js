22a2bc84e59373d9243421332756537ac11d99b0
QuentinM <59691442+Im-Rises@users.noreply.github.com>
2022-04-23 23:01:57 +0200

    Added chaijs test

--- tsp/App/Genetics/repair.js
+++ tsp/App/Genetics/repair.js
@@ -12,12 +12,3 @@ const repair = R.converge(R.concat, [R.uniq, getMissingCity]);
 const repairPopulation = (population) => R.map(repair, population);
 
 export {repairPopulation};
-
-// console.log(
-//   repairPopulation([
-//     [0, 1, 1, 3, 4, 5, 6, 7, 8, 9],
-//     [0, 9, 9, 7, 6, 5, 4, 3, 2, 1],
-//     [0, 1, 2, 2, 4, 5, 6, 7, 8, 9],
-//     [0, 9, 8, 7, 6, 5, 7, 3, 2, 1]
-//   ])
-// );
