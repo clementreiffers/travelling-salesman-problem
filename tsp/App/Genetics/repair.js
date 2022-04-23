86edf6b8ff283dcb1aebf939430715f2b088868a
QuentinM <59691442+Im-Rises@users.noreply.github.com>
2022-04-23 13:51:07 +0200

    Repair wip

--- tsp/App/Genetics/repair.js
+++ tsp/App/Genetics/repair.js
@@ -1,9 +1,21 @@
-// // Repair :
-//
-// const repair = ()=> R.pipe(R.uniq, R.difference())
+import * as R from 'ramda';
+import {append} from 'ramda';
 
-// Mutate, Crossover and Repair
-//
-// const nextGen = R.map(
-//   R.pipe(mutatePopulation, crossoverPopulation, repairPopulation)
+// Repair :
+
+const listCities = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const getMissingCity = R.pipe(R.uniq, R.difference(listCities));
+
+const repair = R.converge(R.concat, [R.uniq, getMissingCity]);
+
+const repairPopulation = (population) => R.map(repair, population);
+
+// console.log(
+//   repairPopulation([
+//     [1, 1, 3, 4, 5, 6, 7, 8, 9],
+//     [9, 9, 7, 6, 5, 4, 3, 2, 1],
+//     [1, 2, 2, 4, 5, 6, 7, 8, 9],
+//     [9, 8, 7, 6, 5, 7, 3, 2, 1]
+//   ])
 // );
