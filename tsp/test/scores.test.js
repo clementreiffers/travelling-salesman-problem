1edd2dcfb620ea9d8bd1db38553d3de48a2c2e30
QuentinM <59691442+Im-Rises@users.noreply.github.com>
2022-04-24 11:53:22 +0200

    All xo corrections done

--- tsp/test/scores.test.js
+++ tsp/test/scores.test.js
@@ -1,9 +1,9 @@
-import {sortListByDist, sortListByScores} from '../App/Genetics/scores.js';
 import {expect} from 'chai';
+import {sortListByDist, sortListByScores} from '../App/Genetics/scores.js';
 
 const map = {0: {x: 0, y: 0, value: 0}, 1: {x: 1, y: 1, value: 1}};
 const population = [{order: [0, 0]}, {order: [0, 1]}];
-const sortedPop = sortListByDist(map)(population);
+// Tconst sortedPop = sortListByDist(map)(population);
 
 describe('../App/Genetics/scores.js', () => {
   describe('#sortListByDist()', () => {
