d809b8c63145713e265b99c73a3494fc9c311cde
QuentinM <59691442+Im-Rises@users.noreply.github.com>
2022-04-25 17:16:25 +0200

    Formatted all files to LF instead of CRLF

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const MAX_POPULATION = 100;
 // Tconst MAX_GEN = 10;
 
 // Tconst map = {0: {x: 0, y: 0, value: 0}, 1: {x: 1, y: 1, value: 1}};
+
 // Tconst population = [{order: [0, 0]}, {order: [0, 1]}];
 
 // Tconsole.log(sortListByDist(map)(population));
