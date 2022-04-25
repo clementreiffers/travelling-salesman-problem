5d17a5e184288bbd5117e6c1034b233eba7f628c
QuentinM <59691442+Im-Rises@users.noreply.github.com>
2022-04-25 15:33:54 +0200

    Added version 2

--- App/Genetics/crossover.js
+++ App/Genetics/crossover.js
@@ -4,6 +4,10 @@ import {getRandomIndex} from './common-functions.js';
 // Crossover
 const crossoverProbability = 1;
 
+const percentParentsDeletion = 4 / 10;
+
+const percentOffspringDeletion = 1 - percentParentsDeletion;
+
 const shouldCrossOver = (proba) => R.pipe(Math.random, R.lt(1 - proba));
 
 const doCrossOver = (reference, target, index) =>
@@ -27,6 +31,25 @@ const createOffsprings = (population) =>
   )(population);
 
 const crossOverPopulation = (population) =>
-  R.concat(createOffsprings(population))(population);
+  R.concat(
+    R.slice(
+      0,
+      Math.ceil(population.length * percentOffspringDeletion),
+      createOffsprings(population)
+    )
+  )(
+    R.slice(
+      0,
+      Math.ceil(population.length * percentParentsDeletion),
+      population
+    )
+  );
+
+console.log(
+  crossOverPopulation([
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [60, 70, 80, 90, 100, 110, 120, 130, 140]
+  ])
+);
 
 export {crossOverPopulation};
