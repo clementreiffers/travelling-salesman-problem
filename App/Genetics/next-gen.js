c8ac0240c1f1163065088af07b6ca79321c04c39
QuentinM <59691442+Im-Rises@users.noreply.github.com>
2022-05-04 00:57:10 +0200

    wip

--- App/Genetics/next-gen.js
+++ App/Genetics/next-gen.js
@@ -4,19 +4,19 @@ import {crossOverPopulation} from './crossover.js';
 import {repairPopulation} from './repair.js';
 import {createPop, createPopulationFromListOfOrder} from './population.js';
 import {sortListByDist} from './scores.js';
-import {shuffleList} from './common-functions.js';
+// import {shuffleList} from './common-functions.js';
+//
+// const sixtyPercent = (pop) =>
+//   Math.floor(R.divide(R.multiply(60, R.length(pop)), 100));
+//
+// const killRandomPeople = (pop) =>
+//   R.slice(0, sixtyPercent(pop), shuffleList(pop));
+//
+// const killSixtyPercent = (pop) => R.slice(0, sixtyPercent(pop), pop);
+//
+// const cloneTheBestIndiv = (pop) => R.append(R.head(pop), pop);
 
-const sixtyPercent = (pop) =>
-  Math.floor(R.divide(R.multiply(60, R.length(pop)), 100));
-
-const killRandomPeople = (pop) =>
-  R.slice(0, sixtyPercent(pop), shuffleList(pop));
-
-const killSixtyPercent = (pop) => R.slice(0, sixtyPercent(pop), pop);
-
-const cloneTheBestIndiv = (pop) => R.append(R.head(pop), pop);
-
-const nextGeneration = (map) => (maxPop) => (maxCities) =>
+const nextGeneration = (map) =>
   R.pipe(
     sortListByDist(map),
     R.pluck('order'),
--- index.js
+++ index.js
@@ -2,39 +2,22 @@ import * as R from 'ramda';
 import {createPop} from './App/Genetics/population.js';
 import {createMap} from './App/Genetics/map.js';
 import nextGeneration from './App/Genetics/next-gen.js';
-import {sortListByDist} from './App/Genetics/scores.js';
 
 const MAX_CITIES = 10;
 const MAX_POPULATION = 100;
+const numberOfIteration = 1;
 
 const map = createMap(MAX_CITIES);
 let population = createPop(MAX_POPULATION)(MAX_CITIES);
 
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
-population = sortListByDist(map)(population);
-// console.log('first step', R.head(population));
+// const getJson = nextGeneration(map)(population);
+//
+// const repeat = R.repeat(getJson, numberOfIteration);
+//
+// console.log(repeat);
 
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
-population = nextGeneration(map)(MAX_POPULATION)(MAX_CITIES)(population);
+for (let i = 0; i < numberOfIteration; i++) {
+  population = nextGeneration(map)(population);
+}
 
 console.log(population);
