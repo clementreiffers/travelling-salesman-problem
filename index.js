b7550641ffa60b939493e014aaba0e9dbeffeab7
QuentinM <59691442+Im-Rises@users.noreply.github.com>
2022-05-05 01:56:46 +0200

    Added tests

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import nextGeneration from './App/Genetics/next-gen.js';
 const MAX_CITIES = 10;
 const MAX_POPULATION = 10;
 const MAX_DISTANCE = 150;
-const numberOfIteration = 100;
+const numberOfIteration = 10;
 
 const map = createMap(MAX_CITIES);
 let population = createPop(MAX_POPULATION)(MAX_CITIES);
@@ -16,13 +16,40 @@ let population = createPop(MAX_POPULATION)(MAX_CITIES);
 //
 // for (let i = 0; i < numberOfIteration; i++) {
 //   population = nextGeneration(map)(MAX_DISTANCE)(population);
+//   console.log(R.head(population));
 // }
-//
 // console.log('Last iteration (' + numberOfIteration + ') :', R.head(population));
 
-const testRepeat = R.repeat(
-  R.head(nextGeneration(map)(MAX_DISTANCE)(population)),
-  numberOfIteration
-);
+// const testRepeat = R.repeat(
+//   R.head(nextGeneration(map)(MAX_DISTANCE)(population)),
+//   numberOfIteration
+// );
+//
+// console.log(testRepeat); //Retourne le meilleur résultat de chaque itération !!!
 
-console.log(R.uniq(testRepeat));
+const pasBeau = (index) => {
+  // console.log(pop === population);
+  console.log(index);
+  population = nextGeneration(map)(MAX_DISTANCE)(population);
+  return population;
+};
+
+const testRepeat = R.times(pasBeau(population), numberOfIteration);
+
+console.log(testRepeat); //Retourne le meilleur résultat de chaque itération !!!
+
+// R.repeat() // ne fonctionne pas correctement car il appelle la fonction envoyé en paramètre qu'une sule fois ce con. Ce qui fait que ça met à jour la population qu'une fois mdr
+
+// console.log(R.head(testRepeat)); //Retourne le meilleur résultat de chaque itération !!!
+
+// console.log(testRepeat);
+
+// let test = [1,2,3,4,5,6,7,8,9];
+//
+// const testfunc = (tab)=>{
+//   tab[2]=-10;
+// }
+//
+// testfunc(test)
+//
+// console.log(test)
