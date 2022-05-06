298a6da0c819ea5fadd2a2edbe609d26c3b80f59
QuentinM <59691442+Im-Rises@users.noreply.github.com>
2022-05-06 14:26:14 +0200

    deleted useless comments

--- index.js
+++ index.js
@@ -11,45 +11,14 @@ const numberOfIteration = 10;
 const map = createMap(MAX_CITIES);
 let population = createPop(MAX_POPULATION)(MAX_CITIES);
 
-// population = nextGeneration(map)(MAX_DISTANCE)(population);
-// console.log('First iteration :', R.head(population));
-//
-// for (let i = 0; i < numberOfIteration; i++) {
-//   population = nextGeneration(map)(MAX_DISTANCE)(population);
-//   console.log(R.head(population));
-// }
-// console.log('Last iteration (' + numberOfIteration + ') :', R.head(population));
-
-// const testRepeat = R.repeat(
-//   R.head(nextGeneration(map)(MAX_DISTANCE)(population)),
-//   numberOfIteration
-// );
-//
-// console.log(testRepeat); //Retourne le meilleur résultat de chaque itération !!!
-
-const pasBeau = (index) => {
-  // console.log(pop === population);
-  console.log(index);
+const doCyle = () => {
   population = nextGeneration(map)(MAX_DISTANCE)(population);
   return population;
 };
 
-const testRepeat = R.times(pasBeau(population), numberOfIteration);
+const calculateTsp = R.times(doCyle, numberOfIteration);
 
-console.log(testRepeat); //Retourne le meilleur résultat de chaque itération !!!
-
-// R.repeat() // ne fonctionne pas correctement car il appelle la fonction envoyé en paramètre qu'une sule fois ce con. Ce qui fait que ça met à jour la population qu'une fois mdr
-
-// console.log(R.head(testRepeat)); //Retourne le meilleur résultat de chaque itération !!!
-
-// console.log(testRepeat);
-
-// let test = [1,2,3,4,5,6,7,8,9];
-//
-// const testfunc = (tab)=>{
-//   tab[2]=-10;
-// }
-//
-// testfunc(test)
-//
-// console.log(test)
+//First iteration best result
+console.log(R.head(R.head(calculateTsp)));
+//Last iteration best result
+console.log(R.head(R.last(calculateTsp)));
