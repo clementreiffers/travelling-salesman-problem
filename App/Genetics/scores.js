d670af950bbac8449586944f58323852a6a83602
clementreiffers <reiffersclement@gmail.com>
2022-05-06 14:35:35 +0100

    bug calcul score corrige

--- App/Genetics/scores.js
+++ App/Genetics/scores.js
@@ -7,18 +7,25 @@ const sqrt = (n) => n ** (1 / 2);
 const subtractAxis = (city1, city2) => (axis) =>
   R.subtract(R.prop(axis, city1), R.prop(axis, city2));
 
-const distance_ = (city1, city2) =>
-  sqrt(
+const distance_ = (city1, city2) => {
+  if (city1 === 0) return 0;
+
+  return sqrt(
     R.add(
       pow(subtractAxis(city1, city2)('x')),
       pow(subtractAxis(city1, city2)('y'))
     )
   );
+};
+
+const isDistanceLessThanTheMax = (value, constraint) => {
+  return R.lt(value, constraint);
+};
 
 const computeScoreWithConstraint = (cities, constraint) =>
   R.pipe(
     R.reduceWhile(
-      (acc) => R.lt(R.prop('distance', acc), constraint),
+      (acc) => isDistanceLessThanTheMax(R.prop('distance', acc), constraint),
       (acc, x) =>
         R.pipe(
           R.over(
@@ -34,12 +41,11 @@ const computeScoreWithConstraint = (cities, constraint) =>
       {
         distance: 0,
         score: 0,
-        currentPosition: undefined
+        currentPosition: 0
       }
     ),
     R.prop('score')
   );
-
 const getScoreFromPopulation = (cities, constraint) =>
   R.map(
     R.applySpec({
