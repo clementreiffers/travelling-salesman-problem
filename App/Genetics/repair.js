0c85ff4a719d8dbd15641e8967cdd884823346a8
QuentinM <59691442+Im-Rises@users.noreply.github.com>
2022-05-09 16:37:21 +0200

    Change functions names from crossover, mutation and repair files

--- App/Genetics/repair.js
+++ App/Genetics/repair.js
@@ -2,13 +2,13 @@ import * as R from 'ramda';
 
 // Repair :
 
-const listCities = (individual) => R.times(R.identity, individual.length);
+const listCities_ = (individual) => R.times(R.identity, individual.length);
 
-const getMissingCity = (individual) =>
-  R.pipe(R.uniq, R.difference(listCities(individual)))(individual);
+const getMissingCity_ = (individual) =>
+  R.pipe(R.uniq, R.difference(listCities_(individual)))(individual);
 
-const repair = R.converge(R.concat, [R.uniq, getMissingCity]);
+const repair_ = R.converge(R.concat, [R.uniq, getMissingCity_]);
 
-const repairPopulation = (population) => R.map(repair, population);
+const repairPopulation = (population) => R.map(repair_, population);
 
 export {repairPopulation};
