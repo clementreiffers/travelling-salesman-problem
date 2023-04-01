e5381a53044f845dad90f6a5682ce4a8cdb4dcda
clementreiffers <reiffersclement@gmail.com>
2023-04-01 17:23:41 +0200

    tests unitaires ok

--- test/map.test.js
+++ test/map.test.js
@@ -4,7 +4,9 @@ import {createMap} from '../App/Genetics/map.js';
 describe('../App/Genetics/map.js', () => {
   describe('#createMap()', () => {
     it('should return a map with coordinate of cities', () => {
-      expect(createMap(10)).to.be.an('Object');
+      expect(createMap({maxCities: 10, width: 10, height: 10})).to.be.an(
+        'Object'
+      );
 
       it.skip('should return an error', () => {});
     });
