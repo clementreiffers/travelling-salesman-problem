465b001837261bc8debb75317274b304023f8ffd
clementreiffers <reiffersclement@gmail.com>
2023-04-01 14:00:10 +0200

    amelioration distance

--- App/Genetics/map.js
+++ App/Genetics/map.js
@@ -1,21 +1,19 @@
 import * as R from 'ramda';
 import {getRandomValue} from './common-functions.js';
 
-const appendCityToMap_ = (map) => (maxCities, width, height) => (city) =>
-  R.assoc(city, createCity_(maxCities, width, height), map);
+const appendCityToMap_ = (map) => (city) =>
+  R.assoc(city, createCity_(map), map);
 
-const createCity_ = (maxCities, width, height) =>
-  R.applySpec({
-    x: R.pipe(getRandomValue, R.multiply(width)),
-    y: R.pipe(getRandomValue, R.multiply(height)),
-    value: R.pipe(getRandomValue, R.multiply(maxCities))
-  })(maxCities);
+const createCity_ = (map) =>
+  R.pipe(
+    R.applySpec({
+      x: R.pipe(getRandomValue, R.multiply(R.prop('width', map))),
+      y: R.pipe(getRandomValue, R.multiply(R.prop('height', map))),
+      value: R.pipe(getRandomValue, R.multiply(R.prop('max', map)))
+    })
+  )(map);
 
-const createMap = (maxCities, width, height) =>
-  R.mergeAll(
-    R.times(
-      appendCityToMap_({max: maxCities})(maxCities, width, height),
-      maxCities
-    )
-  );
+const createMap = (max, width, height) =>
+  R.mergeAll(R.times(appendCityToMap_({max, width, height}), max));
+
 export {createMap};
